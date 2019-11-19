6d6ece9ccd5a4e7c84d34d633d13dd4a104e6ddf
huxiaocheng <12599458@qq.com>
2019-11-20 00:01:34 +0800

    dom编写

--- src/views/img-label/components/img-label.vue
+++ src/views/img-label/components/img-label.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="wrap">
-    <img :src="imgUrl" />
+    <img
+      class="img"
+      :src="imgUrl"
+      :width="imgWidth"
+      :draggable="false"
+      @click="isShowMenu = false"
+      @contextmenu.prevent="handleContextmenu"
+    />
+    <div class="label"></div>
+    <ul class="menu-list" v-show="isShowMenu" ref="menu">
+      <li class="menu-item" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">新建标签</li>
+      <li class="menu-item" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">保存标签</li>
+      <li class="menu-item" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">清除标签</li>
+      <li class="menu-item" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">隐藏标签</li>
+      <li class="menu-item" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">导出为图片</li>
+    </ul>
   </div>
 </template>
 
@@ -15,13 +30,36 @@ export default {
       type: [Number, String],
       default: 500
     },
-    bgColor: {
+    themeColor: {
       type: String,
       default: "#3b8bcc"
+    }
+  },
+  data() {
+    return {
+      isShowMenu: false
+    };
+  },
+  computed: {
+    imgWidth() {
+      return typeof this.width === "number"
+        ? this.width
+        : this.width.slice(0, -2);
+    }
+  },
+  methods: {
+    handleContextmenu(e) {
+      this.isShowMenu = true;
+      console.log(e);
+      // this.$refs['menu'].style.left =
     },
-    textColor: {
-      type: String,
-      default: "#fff"
+    onMouseEnter(e) {
+      e.target.style.backgroundColor = this.themeColor;
+      e.target.style.color = "#fff";
+    },
+    onMouseLeave(e) {
+      e.target.style.backgroundColor = "#fff";
+      e.target.style.color = "#000";
     }
   }
 };
@@ -31,5 +69,62 @@ export default {
 .wrap {
   display: inline-block;
   position: relative;
+
+  .img {
+    display: block;
+  }
+
+  .label {
+    position: absolute;
+    padding: 7px 10px;
+    background: RGBA(0, 0, 0, 0.35);
+    color: #fff;
+    border-radius: 5px;
+    cursor: pointer;
+    user-select: none;
+    outline: none;
+
+    &::before, &::after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 0;
+      height: 0px;
+      border-width: 3px;
+      border-style: solid;
+      color: RGBA(0, 0, 0, 0.35);
+    }
+
+    &::after {
+      border-left-color: transparent;
+      border-bottom-color: transparent;
+      top: 50%;
+      right: 100%;
+    }
+
+    &::before {
+      border-left-color: transparent;
+      border-top-color: transparent;
+      bottom: 50%;
+      right: 100%;
+    }
+  }
+
+  .menu-list {
+    position: absolute;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    cursor: pointer;
+
+    .menu-item {
+      padding: 10px;
+      font-size: 14px;
+      transition: 0.2s all;
+    }
+
+    .menu-item + .menu-item {
+      border-top: 1px solid #ccc;
+    }
+  }
 }
 </style>
--- src/views/img-label/index.vue
+++ src/views/img-label/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img-label :imgUrl="require('./components/img/timg.jpg')"></img-label>
+    <img-label style="margin: 50px;" :imgUrl="require('./components/img/timg.jpg')"></img-label>
     <ImgLabelOld></ImgLabelOld>
   </div>
 </template>
