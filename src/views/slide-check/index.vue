92f0eab3b6d1b1e33ef289a64e1edeefb950c882
huxiaocheng <12599458@qq.com>
2019-11-25 23:29:22 +0800

    图片绘制

--- src/views/slide-check/index.vue
+++ src/views/slide-check/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <canvas ref="canvas" style="border: 1px solid #ccc; margin: 50px;"></canvas>
+  <div>
+    <canvas
+      ref="canvas"
+      @mousedown.prevent="onMousedown"
+      @mousemove.prevent="onMousemove"
+      @mouseup.prevent="onMouseup"
+      @mouseout.prevent="onMouseout"
+      style="border: 1px solid #ccc; margin: 50px;"
+    ></canvas>
+    <canvas ref="watermark-canvas" style="display:none;margin:0;border: 1px solid #aaa"></canvas>
+  </div>
 </template>
 
 <script>
@@ -10,29 +20,72 @@ export default {
   methods: {
     initCanvas() {
       const canvas = this.$refs["canvas"];
-      canvas.width = 800;
-      canvas.height = 800;
+      canvas.width = 400;
+      canvas.height = 400;
       const ctx = canvas.getContext("2d");
-      this.drawRoundRect(ctx, 100, 100, 600, 500, 50);
+
+      this.watermarkCanvas = this.$refs["watermark-canvas"];
+      this.watermarkCanvasCtx = this.watermarkCanvas.getContext("2d");
+
+      this.img = new Image();
+      this.isMouseDown = false;
+      this.img.src = require("./components/img/timg.jpg");
+      this.img.onload = () => {
+        this.watermarkCanvas.width = this.img.width;
+        this.watermarkCanvas.height = this.img.height;
+        this.scale = this.watermarkCanvas.width / canvas.width;
+        ctx.drawImage(this.img, 0, 0, canvas.width, canvas.height);
+        this.watermarkCanvasCtx.drawImage(this.img, 0, 0);
+      };
     },
-    drawRoundRect(ctx, x, y, width, height, radius) {
+    onMousedown(e) {
+      this.bound = this.$refs["canvas"].getBoundingClientRect();
+      this.isMouseDown = true;
+      this.left = e.clientX - this.bound.left;
+      this.top = e.clientY - this.bound.top;
+      this.drawMagnifier(true, this.left, this.top);
+    },
+    onMousemove(e) {
+      if (!this.isMouseDown) return;
+      this.left = e.clientX - this.bound.left;
+      this.top = e.clientY - this.bound.top;
+      this.drawMagnifier(true, this.left, this.top);
+    },
+    onMouseup() {
+      this.isMouseDown = false;
+      this.drawMagnifier(false);
+    },
+    onMouseout() {
+      this.isMouseDown = false;
+      this.drawMagnifier(false);
+    },
+    drawMagnifier(flag, left, top) {
+      if (!flag) return;
+
+      const canvas = this.$refs["canvas"];
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(this.img, 0, 0, canvas.width, canvas.height);
+      const imgMaxX = left * this.scale;
+      const imgMaxY = top * this.scale;
+      const mr = 50;
+      const sx = imgMaxX - mr;
+      const sy = imgMaxY - mr;
+      const dx = left - mr;
+      const dy = top - mr;
       ctx.save();
-      ctx.translate(x, y);
-      this.pathRoundRect(ctx, width, height, radius);
-      ctx.strokeStyle = "#000";
-      ctx.stroke();
+      ctx.drawImage(
+        this.watermarkCanvas,
+        sx,
+        sy,
+        2 * mr,
+        2 * mr,
+        dx,
+        dy,
+        2 * mr,
+        2 * mr
+      );
       ctx.restore();
-    },
-    pathRoundRect(ctx, width, height, radius) {
-      ctx.beginPath();
-      ctx.arc(width - radius, height - radius, radius, 0, Math.PI / 2);
-      ctx.lineTo(radius, height);
-      ctx.arc(radius, height - radius, radius, Math.PI / 2, Math.PI);
-      ctx.lineTo(0, radius);
-      ctx.arc(radius, radius, radius, Math.PI, (Math.PI * 3) / 2);
-      ctx.lineTo(width - radius, 0);
-      ctx.arc(width - radius, radius, radius, (Math.PI * 3) / 2, Math.PI * 2);
-      ctx.closePath();
     }
   }
 };
