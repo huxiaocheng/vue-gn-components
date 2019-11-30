37d54ae7a8e4c120203e1b06aeeab08b85b36311
huxiaocheng <12599458@qq.com>
2019-11-30 15:35:14 +0800

    优化

--- src/views/slide-check/index.vue
+++ src/views/slide-check/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="wrap">
+    <router-link to="/home" tag="button">去首页</router-link>
     <slider-check :src="src" @verifiedPass="success"></slider-check>
   </div>
 </template>
