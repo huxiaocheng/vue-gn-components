201886af0da470b818cd0a02cbc290d407d5dcba
huxiaocheng <12599458@qq.com>
2019-12-02 22:20:27 +0800

    添加水印

--- src/views/watermark/components/watermark.vue
+++ src/views/watermark/components/watermark.vue
@@ -31,44 +31,44 @@ export default {
       ctx.arc(150, 50, 40, 0, 0.5 * Math.PI, true);
       ctx.closePath();
       ctx.stroke();
+    },
+    initCanvas() {
+      const canvas = this.$refs["canvas"];
+      canvas.width = 400;
+      canvas.height = 400;
+      const ctx = canvas.getContext("2d");
+      this.watermarkCanvas = this.$refs["watermark-canvas"];
+      this.watermarkCanvasCtx = this.watermarkCanvas.getContext("2d");
+      const img = new Image();
+      const slider = this.$refs["scale-range"];
+      img.src = require("./img/timg.jpg");
+      img.onload = () => {
+        slider.addEventListener("mousemove", e => {
+          const scale = e.target.value;
+          this.drawImageByScale(scale, canvas, ctx, img);
+        });
+      };
+      this.watermarkCanvas.width = 600;
+      this.watermarkCanvas.height = 100;
+      this.watermarkCanvasCtx.font = "bold 50px Arial";
+      this.watermarkCanvasCtx.lineWidth = "1";
+      this.watermarkCanvasCtx.fillStyle = "rgba(0,0,0,0.5)";
+      this.watermarkCanvasCtx.textBaseLine = "middle";
+      this.watermarkCanvasCtx.fillText("==hello world==", 20, 50);
+    },
+    drawImageByScale(scale, canvas, ctx, img) {
+      const imgWidth = 400 * scale;
+      const imgHeight = 400 * scale;
+      const dx = canvas.width / 2 - imgWidth / 2;
+      const dy = canvas.height / 2 - imgHeight / 2;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(img, dx, dy, imgWidth, imgHeight);
+      ctx.drawImage(
+        this.watermarkCanvas,
+        canvas.width - this.watermarkCanvas.width,
+        canvas.height - this.watermarkCanvas.height
+      );
     }
-    // initCanvas() {
-    //   const canvas = this.$refs["canvas"];
-    //   canvas.width = 400;
-    //   canvas.height = 400;
-    //   const ctx = canvas.getContext("2d");
-    //   this.watermarkCanvas = this.$refs["watermark-canvas"];
-    //   this.watermarkCanvasCtx = this.watermarkCanvas.getContext("2d");
-    //   const img = new Image();
-    //   const slider = this.$refs["scale-range"];
-    //   img.src = require("./img/timg.jpg");
-    //   img.onload = () => {
-    //     slider.addEventListener("mousemove", e => {
-    //       const scale = e.target.value;
-    //       this.drawImageByScale(scale, canvas, ctx, img);
-    //     });
-    //   };
-    //   this.watermarkCanvas.width = 600;
-    //   this.watermarkCanvas.height = 100;
-    //   this.watermarkCanvasCtx.font = "bold 50px Arial";
-    //   this.watermarkCanvasCtx.lineWidth = "1";
-    //   this.watermarkCanvasCtx.fillStyle = "rgba(0,0,0,0.5)";
-    //   this.watermarkCanvasCtx.textBaseLine = "middle";
-    //   this.watermarkCanvasCtx.fillText("==hello world==", 20, 50);
-    // },
-    // drawImageByScale(scale, canvas, ctx, img) {
-    //   const imgWidth = 400 * scale;
-    //   const imgHeight = 400 * scale;
-    //   const dx = canvas.width / 2 - imgWidth / 2;
-    //   const dy = canvas.height / 2 - imgHeight / 2;
-    //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //   ctx.drawImage(img, dx, dy, imgWidth, imgHeight);
-    //   ctx.drawImage(
-    //     this.watermarkCanvas,
-    //     canvas.width - this.watermarkCanvas.width,
-    //     canvas.height - this.watermarkCanvas.height
-    //   );
-    // }
   }
 };
 </script>
