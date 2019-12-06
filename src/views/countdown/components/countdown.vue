ce9a3a13baadd78a9c23bf4d2880e33c20d0a183
huxiaocheng <12599458@qq.com>
2019-12-07 00:45:46 +0800

    时间格式化

--- src/views/countdown/components/countdown.vue
+++ src/views/countdown/components/countdown.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div>
+    {{date.year}}
+    {{date.month}}
+    {{date.date}}
+    {{date.hours}}
+    {{date.minutes}}
+    {{date.seconds}}
+  </div>
 </template>
 
 <script>
@@ -17,7 +24,7 @@ export default {
   },
   data() {
     return {
-      time: {
+      date: {
         year: "",
         month: "",
         date: "",
@@ -42,20 +49,55 @@ export default {
       } else if ((this.time + "").length === 13) {
         time = new Date(this.time);
       }
-      this.normalizeTime(time);
+      time = time.getTime() - new Date().getTime();
+      this.mySetInterval(() => {
+        time -= 1000;
+        this.normalizeTime(time);
+      }, 1000);
     },
     normalizeTime(time) {
-      const D = new Date(time);
-      this.time.year = D.getFullYear();
-      this.time.month = D.getMonth() + 1;
-      this.time.date = D.getDate();
-      this.time.hours = D.getHours();
-      this.time.Minutes = D.getMinutes();
-      this.time.seconds = D.getSeconds();
-      this.time.milliseconds = D.getMilliseconds();
+      time = time / 1000;
+      const oneYear = 60 * 60 * 24 * 365;
+      const oneMonth = 60 * 60 * 24 * 30;
+      const oneDay = 60 * 60 * 24;
+      const oneHours = 60 * 60;
+      const oneMinutes = 60;
+      if (time > oneYear) {
+        this.date.year = Math.floor(time / oneYear);
+      }
+      if (time > oneMonth) {
+        this.date.month = Math.floor((time % oneYear) / oneMonth);
+      }
+      if (time > oneDay) {
+        this.date.date = Math.floor(((time % oneYear) % oneMonth) / oneDay);
+      }
+      if (time > oneHours) {
+        this.date.hours = Math.floor(
+          (((time % oneYear) % oneMonth) % oneDay) / oneHours
+        );
+      }
+      if (time > oneMinutes) {
+        this.date.minutes = Math.floor(
+          ((((time % oneYear) % oneMonth) % oneDay) % oneHours) / oneMinutes
+        );
+      }
+      if (time > 1) {
+        this.date.seconds = Math.floor(
+          (((((time % oneYear) % oneMonth) % oneDay) % oneHours) % oneMinutes) /
+            1
+        );
+      }
+    },
+    mySetInterval(fn, delay) {
+      let timer;
+      const loop = (fn, delay) => {
+        timer = setTimeout(() => {
+          loop(fn, delay);
+          fn.call(this, timer);
+        }, delay);
+      };
+      loop(fn, delay);
     }
   }
 };
 </script>
-
-<style lang="stylus" scoped></style>
--- src/views/countdown/index.vue
+++ src/views/countdown/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    index Countdown
+    <countdown :time="'2019-11-7-13-10-55'"></countdown>
     <router-link to="/home" tag="button">去首页</router-link>
   </div>
 </template>
 
 <script>
-export default {};
+import Countdown from "./components/countdown";
+export default {
+  components: {
+    Countdown
+  }
+};
 </script>
 
 <style lang="stylus" scoped></style>
