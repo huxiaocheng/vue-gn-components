0cb11c455342824658fdc537bcb21e3d037c408e
huxiaocheng <12599458@qq.com>
2019-12-07 12:34:28 +0800

    倒计时初步完成

--- src/views/countdown/components/countdown.vue
+++ src/views/countdown/components/countdown.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    {{date.year}}
-    {{date.month}}
-    {{date.date}}
-    {{date.hours}}
-    {{date.minutes}}
-    {{date.seconds}}
+    <slot :date="date" />
   </div>
 </template>
 
@@ -17,43 +12,75 @@ export default {
       default() {
         return new Date();
       }
+    },
+    isSMSMode: {
+      type: Boolean,
+      default: false
+    },
+    totalTime: {
+      type: [Number, String],
+      default: 0
     }
   },
   created() {
-    this.initTime();
+    if (!this.isSMSMode || localStorage.getItem("__SMS_total_time__") > 0) {
+      this.start();
+    }
   },
   data() {
     return {
       date: {
-        year: "",
-        month: "",
-        date: "",
-        hours: "",
-        minutes: "",
-        seconds: "",
-        milliseconds: ""
+        year: 0,
+        month: 0,
+        date: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+        timeLeft: localStorage.getItem("__SMS_total_time__") || this.totalTime
       }
     };
   },
   methods: {
+    start() {
+      this.initTime();
+    },
+    pause() {
+      clearTimeout(this.timer);
+    },
     initTime() {
-      let time;
-      if (Object.prototype.toString.call(this.time) === "[object Date]") {
-        time = this.time;
-      } else if (typeof this.time === "string") {
-        if (this.time.indexOf("-") !== -1) {
-          time = new Date(...this.time.split("-"));
-        } else if (this.time.indexOf(".") !== -1) {
-          time = new Date(...this.time.split("."));
+      if (this.isSMSMode && this.date.timeLeft > 0) {
+        this.mySetInterval(timer => {
+          localStorage.setItem("__SMS_total_time__", (this.date.timeLeft -= 1));
+          if (localStorage.getItem("__SMS_total_time__") <= 0) {
+            this.$emit("end");
+            clearTimeout(timer);
+            localStorage.removeItem("__SMS_total_time__");
+          }
+        }, 1000);
+      } else {
+        let time;
+        if (Object.prototype.toString.call(this.time) === "[object Date]") {
+          time = this.time;
+        } else if (typeof this.time === "string") {
+          if (this.time.indexOf("-") !== -1) {
+            time = new Date(...this.time.split("-"));
+          } else if (this.time.indexOf(".") !== -1) {
+            time = new Date(...this.time.split("."));
+          }
+        } else if ((this.time + "").length === 13) {
+          time = new Date(this.time);
         }
-      } else if ((this.time + "").length === 13) {
-        time = new Date(this.time);
+        time = time.getTime() - new Date().getTime();
+
+        if (time < 0) return;
+
+        this.mySetInterval(timer => {
+          time -= 1000;
+          this.normalizeTime(time);
+          this.timer = timer;
+        }, 1000);
       }
-      time = time.getTime() - new Date().getTime();
-      this.mySetInterval(() => {
-        time -= 1000;
-        this.normalizeTime(time);
-      }, 1000);
     },
     normalizeTime(time) {
       time = time / 1000;
@@ -63,29 +90,51 @@ export default {
       const oneHours = 60 * 60;
       const oneMinutes = 60;
       if (time > oneYear) {
-        this.date.year = Math.floor(time / oneYear);
+        this.date.year =
+          Math.floor(time / oneYear) > 1 ? Math.floor(time / oneYear) : 0;
       }
       if (time > oneMonth) {
-        this.date.month = Math.floor((time % oneYear) / oneMonth);
+        this.date.month =
+          Math.floor((time % oneYear) / oneMonth) > 1
+            ? Math.floor((time % oneYear) / oneMonth)
+            : 0;
       }
       if (time > oneDay) {
-        this.date.date = Math.floor(((time % oneYear) % oneMonth) / oneDay);
+        this.date.date =
+          Math.floor(((time % oneYear) % oneMonth) / oneDay) > 1
+            ? Math.floor(((time % oneYear) % oneMonth) / oneDay)
+            : 0;
       }
       if (time > oneHours) {
-        this.date.hours = Math.floor(
-          (((time % oneYear) % oneMonth) % oneDay) / oneHours
-        );
+        this.date.hours =
+          Math.floor((((time % oneYear) % oneMonth) % oneDay) / oneHours) > 1
+            ? Math.floor((((time % oneYear) % oneMonth) % oneDay) / oneHours)
+            : 0;
       }
       if (time > oneMinutes) {
-        this.date.minutes = Math.floor(
-          ((((time % oneYear) % oneMonth) % oneDay) % oneHours) / oneMinutes
-        );
+        this.date.minutes =
+          Math.floor(
+            ((((time % oneYear) % oneMonth) % oneDay) % oneHours) / oneMinutes
+          ) > 1
+            ? Math.floor(
+                ((((time % oneYear) % oneMonth) % oneDay) % oneHours) /
+                  oneMinutes
+              )
+            : 0;
       }
       if (time > 1) {
-        this.date.seconds = Math.floor(
-          (((((time % oneYear) % oneMonth) % oneDay) % oneHours) % oneMinutes) /
-            1
-        );
+        this.date.seconds =
+          Math.floor(
+            (((((time % oneYear) % oneMonth) % oneDay) % oneHours) %
+              oneMinutes) /
+              1
+          ) > 1
+            ? Math.floor(
+                (((((time % oneYear) % oneMonth) % oneDay) % oneHours) %
+                  oneMinutes) /
+                  1
+              )
+            : 0;
       }
     },
     mySetInterval(fn, delay) {
--- src/views/countdown/index.vue
+++ src/views/countdown/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <countdown :time="'2019-11-7-13-10-55'"></countdown>
+    <div>距离{{date}}还剩下:</div>
+    <countdown :time="date" ref="countdown" :totalTime="5">
+      <template
+        v-slot="{date}"
+      >{{date.year}}年-{{date.month}}月-{{date.date}}日-{{date.hours}}小时-{{date.minutes}}分钟-{{date.seconds}}秒</template>
+    </countdown>
+    <button @click="start">开始</button>
+    <button @click="pause">暂停</button>
     <router-link to="/home" tag="button">去首页</router-link>
   </div>
 </template>
@@ -8,6 +15,19 @@
 <script>
 import Countdown from "./components/countdown";
 export default {
+  data() {
+    return {
+      date: "2022.11.11"
+    };
+  },
+  methods: {
+    pause() {
+      this.$refs["countdown"].pause();
+    },
+    start() {
+      this.$refs["countdown"].start();
+    }
+  },
   components: {
     Countdown
   }
