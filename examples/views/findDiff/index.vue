e65561fbfebac2d94755ce9132ade975c6d8577b
huxiaocheng <12599458@qq.com>
2020-01-11 18:01:55 +0800

    示例修改

--- examples/views/findDiff/index.vue
+++ examples/views/findDiff/index.vue
@@ -1,11 +1,22 @@
 <template>
-<div>
-  <img src='./img/QQ截图20200108124516.png'/>
-  <find-diff />
-  <router-link tag="button" to='/home' @click.native.stop>去首页</router-link>
-</div>
+  <div>
+    <img src='./img/QQ截图20200108124516.png' />
+    <find-diff />
+    <router-link
+      tag="button"
+      to='/home'
+      @click.native.stop="onClick"
+    >去首页</router-link>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    onClick() {
+      const canDraw = document.getElementById("__canvas_diff_");
+      canDraw && document.body.removeChild(canDraw);
+    }
+  }
+};
 </script>
